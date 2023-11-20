5a091bc318c28d07d99f038db7f933722c865ae4
Stefano Verzegnassi <stefano@sverzegnassi.me>
2023-11-20 18:19:15 +0100

    A bit of image optimization

--- src/utils/storyblokUtils.js
+++ src/utils/storyblokUtils.js
@@ -1,6 +1,14 @@
 export function withWebpSupport(url) {
-    if (!url.endsWith('/m/')) {
-        url = url + '/m/';
-    }
-    return url;
+  if (!url.endsWith("/m/")) {
+    url = url + "/m/";
+  }
+  return url;
+}
+
+// Ref. https://www.storyblok.com/faq/image-dimensions-assets-js
+export function imageDimensions(url) {
+  return {
+    width: url.split("/")[5].split("x")[0],
+    height: url.split("/")[5].split("x")[1],
+  };
 }
